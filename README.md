ac3bdb0d57dfe73f87233bda2dd51e7eee033a00
sgsetts <stephen.setterlind@gmail.com>
2022-10-04 01:17:31 -0500

    updated README, commented javascript

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Challenge-3
+
+## Description
+
+
+Having a secure password to protect your data is as important as ever, and this password generator is a great help to that end. Made with JavaScript, it allows the user to select from multiple options to create a completely random password with various letters, number, and symbols.
+
+![screenshot 1](assets/challenge-3.png)
+
+## Usage
+
+Deployed website: [https://sgsetts.github.io/Challenge-3/](https://sgsetts.github.io/Challenge-3/)
+
+## License
+
+MIT License
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// function for generating password
 function generatePassword() {
+
+  // promts user to input password length
   var userInput = window.prompt("Thank you for using Steve's password generator! Choose a password length between 8 and 128 characters: ");
   var passLength  = parseInt(userInput);
 
+  // ensures the user's input is valid
   if (isNaN(passLength)) {
     window.alert("Invalid, please input an integer.");
     return;
@@ -13,24 +17,26 @@ function generatePassword() {
     return;
   }
 
+  // promts user to select password options
   var userNumbers = window.confirm("Would you like to use numbers?");
   var userSymbols = window.confirm("Would you like to use symbols?");
   var userLower = window.confirm("Would you like to use lowercase letters?");
   var userUpper = window.confirm("Would you like to use uppercase letters?");
 
+  // ensures the user's input is valid
   if (userNumbers === false && userSymbols === false && userLower === false && userUpper === false) {
     window.alert("You must select at least one option, try again.");
     return;
   }
 
+  // arrays that store potential options
   var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var symbolArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '=', '<', '>', '/', '-', '+'];
   var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+  // array to store selected options, followed by concats to merge previous arrays into the selectedOptions array
   var selectedOptions = [];
-
-
 
   if (userNumbers === true) {
     selectedOptions = selectedOptions.concat(numberArr);
@@ -45,7 +51,7 @@ function generatePassword() {
     selectedOptions = selectedOptions.concat(upperArr);
   }
 
-
+  // assembles random characters from the selected options into the new password, using the amount of characters the user first selected
   var password = "";
   for (var i = 0; i < passLength; i++) {
     var randIndex = Math.floor(Math.random() * selectedOptions.length);
@@ -55,7 +61,6 @@ function generatePassword() {
   return password;
   
 }
-
 
 // Write password to the #password input
 function writePassword() {
